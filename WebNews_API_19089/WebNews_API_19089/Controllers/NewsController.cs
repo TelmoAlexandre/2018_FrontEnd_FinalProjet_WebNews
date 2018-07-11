3eb56eea47a921433cd19dfbdddbc0a15bc24d9b
Telmo Alexandre <telmo93@gmail.com>
2018-07-11 18:39:20 +0100

    Já é possível pedir apenas uma noticia.

--- WebNews_API_19089/WebNews_API_19089/Controllers/NewsController.cs
+++ WebNews_API_19089/WebNews_API_19089/Controllers/NewsController.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Description;
 using WebNews_API_19089.Models;
 using WebNews_API_19089.Models.ViewModels;
 
 namespace WebNews_API_19089.Controllers
 {
 
-    [RoutePrefix("api/agentes")]
+    [RoutePrefix("api/News")]
     public class NewsController : ApiController
     {
         #region Database
@@ -21,11 +22,13 @@ namespace WebNews_API_19089.Controllers
 
         #region NewsList
 
+        [ResponseType(typeof(GetNewsViewModel))]
         public IHttpActionResult GetNews()
         {
 
             // Utilizo um ViewModel para receber apenas os dados mostrados na página inicial
-            var news = db.News.Select(n => new NewsFrontPageViewModel {
+            var news = db.News.Select(n => new NewsFrontPageViewModel
+            {
                 ID = n.ID,
                 Title = n.Title,
                 Description = n.Description
@@ -33,15 +36,92 @@ namespace WebNews_API_19089.Controllers
 
             // Retorno outro view model que contem um ICollection<NewsFrontPageViewModel>
             // e uma string com a categoria
-            return Ok(new GetNewsViewModel {
+            return Ok(new GetNewsViewModel
+            {
                 News = news,
                 Category = "All"
             });
         }
 
+        [ResponseType(typeof(GetNewsViewModel))]
+        [HttpGet, Route("category/{categoryID}")]
+        public IHttpActionResult GetNews(int categoryID)
+        {
+
+            // Tenta encontrar a categoria
+            var category = db.Categories.Find(categoryID);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            // Encontrar as noticias dessa categoria
+            var news = db.News.Where(n => n.CategoryFK == categoryID).Select(n => new
+            {
+                n.ID,
+                n.Title,
+                n.Description
+            }).ToList();
+
+            return Ok(new
+            {
+                news,
+                category = category.Name
+            });
+        }
 
 
+        #endregion
 
+        #region SingleNews
+
+        [HttpGet, Route("{id}")]
+        public IHttpActionResult GetNewsArticle(int id)
+        {
+
+            var newsArticle = db.News.Find(id);
+
+            if (newsArticle == null)
+            {
+                return NotFound();
+            }
+
+            // Recolher os autores da notícia
+            var users = newsArticle.UsersProfileList.Select(u => new
+            {
+                u.ID,
+                u.Name
+            }).ToList();
+
+            // Recolher os comentários da notícia
+            var comments = newsArticle.CommentsList.Select(c => new
+            {
+                c.CommentDate,
+                c.Content,
+                c.UserProfile.Name,
+                c.UserProfile.ID
+            }).ToList();
+
+            // Recolher as fotos
+            var photos = newsArticle.PhotosList.Select(p => new {
+                p.Name
+            }).ToList();
+
+            return Ok(new
+            {
+                newsArticle.ID,
+                newsArticle.Title,
+                newsArticle.Description,
+                newsArticle.Content,
+                newsArticle.Category.Name,
+                users,
+                comments,
+                photos
+            }
+            );
+
+        }
         #endregion
 
     }
